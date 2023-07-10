8b81f708e183cdd60d55bb1fa5d37580b5c49626
thzero <thzero@users.noreply.github.com>
2023-07-09 21:24:47 -0500

    updates

--- content/parts/part/parachutePartComponent.vue
+++ content/parts/part/parachutePartComponent.vue
@@ -70,9 +70,17 @@ export function useParachutePartComponent(props, context, options) {
 		preCompleteOkPart: (correlationId, data) => {
 			data.cd = Number(detailItemCd.value);
 			data.diameter = Number(detailItemDiameter.value);
+			data.loadMax = Number(detailItemLoadMax.value);
+			data.loadMin = Number(detailItemLoadMin.value);
 			data.thinMill = detailItemThinMill.value ?? false;
+
 			data.diameterMeasurementUnitId = diameterMeasurementUnitId.value;
 			data.diameterMeasurementUnitsId = diameterMeasurementUnitsId.value;
+			
+			data.loadMaxWeightMeasurementUnitId = loadMaxWeightMeasurementUnitId.value;
+			data.loadMaxWeightMeasurementUnitsId = loadMaxWeightMeasurementUnitsId.value;
+			data.loadMinWeightMeasurementUnitId = loadMinWeightMeasurementUnitId.value;
+			data.loadMinWeightMeasurementUnitsId = loadMinWeightMeasurementUnitsId.value;
 
 			const temp = AppUtility.measurementUnitTranslateLength(correlationId, diameterMeasurementUnitsId.value, diameterMeasurementUnitId.value);
 			data.sortName = String(data.diameter ?? '').padStart(4, '0') + temp + (data.thinMill ? 'TM' : '') + data.name;
@@ -86,19 +94,32 @@ export function useParachutePartComponent(props, context, options) {
 	
 	const detailItemCd = ref(null);
 	const detailItemDiameter = ref(null);
+	const detailItemLoadMax = ref(null);
+	const detailItemLoadMin = ref(null);
 	const detailItemThinMill = ref(false);
 	const diameterMeasurementUnitId = ref(null);
 	const diameterMeasurementUnitsId = ref(null);
+	const loadMaxWeightMeasurementUnitId = ref(null);
+	const loadMaxWeightMeasurementUnitsId = ref(null);
+	const loadMinWeightMeasurementUnitId = ref(null);
+	const loadMinWeightMeasurementUnitsId = ref(null);
 
 	const resetData = (correlationId, value) => {
 		detailItemName.value = value ? value.name : LibraryClientUtility.$trans.t('forms.content.parts.parachute.name');
 
 		detailItemCd.value = value ? value.cd : null;
 		detailItemDiameter.value = value ? value.diameter : null;
+		detailItemLoadMax.value = value ? value.loadMax : null;
+		detailItemLoadMin.value = value ? value.loadMin : null;
 		detailItemThinMill.value = value ? value.thinMill ?? false : false;
 		
-		diameterMeasurementUnitId.value = measurementUnitsLengthDefaultId.value;
-		diameterMeasurementUnitsId.value = measurementUnitsIdSettings.value;
+		diameterMeasurementUnitId.value = value ? value.diameterMeasurementUnitId ?? measurementUnitsLengthDefaultId.value : measurementUnitsLengthDefaultId.value;
+		diameterMeasurementUnitsId.value = value ? value.diameterMeasurementUnitsId ?? ementUnitsIdSettings.value : measurementUnitsIdSettings.value;
+		
+		loadMaxWeightMeasurementUnitId.value = value ? value.loadMaxWeightMeasurementUnitId ?? measurementUnitsWeightDefaultId.value : measurementUnitsWeightDefaultId.value;
+		loadMaxWeightMeasurementUnitsId.value = value ? value.loadMaxWeightMeasurementUnitsId ?? measurementUnitsIdSettings.value : measurementUnitsIdSettings.value;
+		loadMinWeightMeasurementUnitId.value = value ? value.loadMinWeightMeasurementUnitId ?? measurementUnitsWeightDefaultId.value : measurementUnitsWeightDefaultId.value;
+		loadMinWeightMeasurementUnitsId.value = value ? value.loadMinWeightMeasurementUnitsId ?? measurementUnitsIdSettings.value : measurementUnitsIdSettings.value;
 	};
 	
 	return {
@@ -153,9 +174,15 @@ export function useParachutePartComponent(props, context, options) {
 		requestManufacturers,
 		detailItemCd,
 		detailItemDiameter,
+		detailItemLoadMax,
+		detailItemLoadMin,
 		detailItemThinMill,
 		diameterMeasurementUnitId,
 		diameterMeasurementUnitsId,
+		loadMaxWeightMeasurementUnitId,
+		loadMaxWeightMeasurementUnitsId,
+		loadMinWeightMeasurementUnitId,
+		loadMinWeightMeasurementUnitsId,
 		scope: 'ParchutePartControl',
 		validation: useVuelidate({ $scope: 'ParchutePartControl' })
 	};
