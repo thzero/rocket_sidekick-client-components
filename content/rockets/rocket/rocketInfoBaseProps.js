5cae94206b49eff4d888fa2b96297581096f98fd
thzero <thzero@users.noreply.github.com>
2023-07-15 22:18:13 -0500

    updates

--- content/rockets/rocket/rocketInfoBaseProps.js
+++
@@ -1,8 +0,0 @@
-import AppCommonConstants from 'rocket_sidekick_common/constants';
-
-export const useRocketInfoBaseProps = {
-		type: {
-			type: String,
-			default: AppCommonConstants.Rocketry.RocketTypes.Site
-		}
-};
--- content/rockets/rocketsBaseProps.js
+++
@@ -1,8 +0,0 @@
-import AppCommonConstants from 'rocket_sidekick_common/constants';
-
-export const useRocketsBaseProps = {
-	type: {
-		type: String,
-		default: AppCommonConstants.Rocketry.DisplayTypes.Site
-	}
-};
