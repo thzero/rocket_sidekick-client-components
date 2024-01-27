13cd05d864036ddb029a96bd290bb3b89566f569
thzero <thzero@users.noreply.github.com>
2024-01-27 09:11:34 -0600

    updates

--- content/linksBase.vue
+++ content/linksBase.vue
@@ -1,5 +1,6 @@
 <script>
 import { computed } from 'vue';
+import { firstBy, thenBy } from 'thenby';
 
 import { useContentBaseComponent } from '@/components/content/contentBase';
 
@@ -30,52 +31,92 @@ export function useLinksBaseComponent(props, context, options) {
 	const linksBooks = computed(() => {
 		if (!links.value)
 			return [];
-		return links.value.filter(l => l.category === 'book');
+		// return links.value.filter(l => l.category === 'book');
+		return links.value.filter(l => l.category === 'book').sort(
+			firstBy((v1, v2) => { return (v1.sortName && v2.sortName) && v1.sortName.localeCompare(v2.sortName); })
+			.thenBy((v1, v2) => { return (v1.name && v2.name) && (v1.name.localeCompare(v2.name)); })
+		);
 	});
 	const linksCompetitions = computed(() => {
 		if (!links.value)
 			return [];
-		return links.value.filter(l => l.category === 'competition');
+		// return links.value.filter(l => l.category === 'competition');
+		return links.value.filter(l => l.category === 'competition').sort(
+			firstBy((v1, v2) => { return (v1.sortName && v2.sortName) && v1.sortName.localeCompare(v2.sortName); })
+			.thenBy((v1, v2) => { return (v1.name && v2.name) && (v1.name.localeCompare(v2.name)); })
+		);
 	});
 	const linksEvents = computed(() => {
 		if (!links.value)
 			return [];
-		return links.value.filter(l => l.category === 'event');
+		// return links.value.filter(l => l.category === 'event');
+		return links.value.filter(l => l.category === 'event').sort(
+			firstBy((v1, v2) => { return (v1.sortName && v2.sortName) && v1.sortName.localeCompare(v2.sortName); })
+			.thenBy((v1, v2) => { return (v1.name && v2.name) && (v1.name.localeCompare(v2.name)); })
+		);
 	});
 	const linksForums = computed(() => {
 		if (!links.value)
 			return [];
-		return links.value.filter(l => l.category === 'forum');
+		// return links.value.filter(l => l.category === 'forum');
+		return links.value.filter(l => l.category === 'forum').sort(
+			firstBy((v1, v2) => { return (v1.sortName && v2.sortName) && v1.sortName.localeCompare(v2.sortName); })
+			.thenBy((v1, v2) => { return (v1.name && v2.name) && (v1.name.localeCompare(v2.name)); })
+		);
 	});
 	const linksGeneral = computed(() => {
 		if (!links.value)
 			return [];
-		return links.value.filter(l => String.isNullOrEmpty(l.category));
+		// return links.value.filter(l => String.isNullOrEmpty(l.category));
+		return links.value.filter(l => String.isNullOrEmpty(l.category)).sort(
+			firstBy((v1, v2) => { return (v1.sortName && v2.sortName) && v1.sortName.localeCompare(v2.sortName); })
+			.thenBy((v1, v2) => { return (v1.name && v2.name) && (v1.name.localeCompare(v2.name)); })
+		);
 	});
 	const linksGuidance = computed(() => {
 		if (!links.value)
 			return [];
-		return links.value.filter(l => l.category === 'guidance');
+		// return links.value.filter(l => l.category === 'guidance');
+		return links.value.filter(l => l.category === 'guidance').sort(
+			firstBy((v1, v2) => { return (v1.sortName && v2.sortName) && v1.sortName.localeCompare(v2.sortName); })
+			.thenBy((v1, v2) => { return (v1.name && v2.name) && (v1.name.localeCompare(v2.name)); })
+		);
 	});
 	const linksManufacturers = computed(() => {
 		if (!links.value)
 			return [];
-		return links.value.filter(l => l.category === 'manufacturer');
+		// return links.value.filter(l => l.category === 'manufacturer');
+		return links.value.filter(l => l.category === 'manufacturer').sort(
+			firstBy((v1, v2) => { return (v1.sortName && v2.sortName) && v1.sortName.localeCompare(v2.sortName); })
+			.thenBy((v1, v2) => { return (v1.name && v2.name) && (v1.name.localeCompare(v2.name)); })
+		);
 	});
 	const linksOrganizations = computed(() => {
 		if (!links.value)
 			return [];
-		return links.value.filter(l => l.category === 'organization');
+		// return links.value.filter(l => l.category === 'organization');
+		return links.value.filter(l => l.category === 'organization').sort(
+			firstBy((v1, v2) => { return (v1.sortName && v2.sortName) && v1.sortName.localeCompare(v2.sortName); })
+			.thenBy((v1, v2) => { return (v1.name && v2.name) && (v1.name.localeCompare(v2.name)); })
+		);
 	});
 	const linksVendors = computed(() => {
 		if (!links.value)
 			return [];
-		return links.value.filter(l => l.category === 'vendor');
+		// return links.value.filter(l => l.category === 'vendor');
+		return links.value.filter(l => l.category === 'vendor').sort(
+			firstBy((v1, v2) => { return (v1.sortName && v2.sortName) && v1.sortName.localeCompare(v2.sortName); })
+			.thenBy((v1, v2) => { return (v1.name && v2.name) && (v1.name.localeCompare(v2.name)); })
+		);
 	});
 	const linksVideos = computed(() => {
 		if (!links.value)
 			return [];
-		return links.value.filter(l => l.category === 'video');
+		// return links.value.filter(l => l.category === 'video');
+		return links.value.filter(l => l.category === 'video').sort(
+			firstBy((v1, v2) => { return (v1.sortName && v2.sortName) && v1.sortName.localeCompare(v2.sortName); })
+			.thenBy((v1, v2) => { return (v1.name && v2.name) && (v1.name.localeCompare(v2.name)); })
+		);
 	});
 
 	return {
