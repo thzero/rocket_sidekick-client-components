784729ae98518b9b040306fbd513f479b58532d8
Corvette Rox <development@thzero.com>
2025-05-22 17:55:36 -0500

    location updates

--- content/locations/locationUtilityComponent.vue
+++ content/locations/locationUtilityComponent.vue
@@ -11,6 +11,7 @@ import LibraryClientUtility from '@thzero/library_client/utility/index';
 import LibraryCommonUtility from '@thzero/library_common/utility/index';
 
 import { useBaseComponent } from '@thzero/library_client_vue3/components/base';
+import { useRocketsUtilityComponent } from '@/components/content/rockets/rocketsUtilityComponent';
 
 export function useLocationsUtilityComponent(props, context, options) {
 	const {
@@ -24,23 +25,45 @@ export function useLocationsUtilityComponent(props, context, options) {
 		notImplementedError,
 		success
 	} = useBaseComponent(props, context, options);
+
+	const {
+		rocketTypeNames
+	} = useRocketsUtilityComponent(props, context, options);
 	
 	const location = ref(null);
 
 	const locationIterations = computed(() => {
 		if (location.value)
-			return LibraryClientVueUtility.selectBlank(location.value.iterations.map(l => { return { id : l.id, name: locationIterationName(l) }; }), '');
+			return LibraryClientVueUtility.selectBlank(location.value.iterations.map(l => { return { 
+				id : l.id, 
+				name: locationIterationName(l)
+			}; 
+		}), '');
 
 		return [];
 	});
 	
 	const locationIterationName = (item) => {
-		let output = '';
+		let output = [];
 		if (item.number)
-			output += '#' + item.number + ' ';
+			output.push(`#${item.number}`);
+		// if (item.name)
+		// 	output.push(item.name);
+
+		if (!item.number && !item.number) {
+			if (item.address) {
+				if (item.address.city)
+					output.push(item.address.city);
+			}
+		}
 		if (item.year)
-			output += item.year + ' ';
-		return output.trim();
+			output.push(item.year);
+
+		if (item.rocketTypes) {
+			output.push(`(${rocketTypeNames(item.rocketTypes)})`);
+		}
+
+		return output.join(' ');
 	};
 
 	return {
