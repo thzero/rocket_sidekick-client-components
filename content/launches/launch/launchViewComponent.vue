b2b57be1495e31f0310b3030293d19aa5c453d8c
Corvette Rox <development@thzero.com>
2025-08-04 17:57:07 -0500

    cleanup

--- content/launches/launch/launchViewComponent.vue
+++ content/launches/launch/launchViewComponent.vue
@@ -149,10 +149,7 @@ export function useLaunchViewComponent(props, context, options) {
 		return displayItem.value.rocketSetup && displayItem.value.rocketSetup.rocket ? displayItem.value.rocketSetup.rocket.coverUrl : null;
 	});
 	const displayItemDate = computed(() => {
-		if (!displayItem.value)
-			return null;
-		// return (displayItem.value.date ? new Date(displayItem.value.date).toLocaleDateString() : '');
-		return (displayItem.value.date ? LibraryMomentUtility.getDateHuman(displayItem.value.date) : '');
+		return launchDate(displayItem.value);
 	});
 	const displayItemLocationLink = computed(() => {
 		if (!displayItem.value || !displayItem.value.location)
@@ -174,122 +171,59 @@ export function useLaunchViewComponent(props, context, options) {
 		return ` (${temp.join(', ')})`;
 	});
 	const displayItemLocationIterationAddress = computed(() => {
-		// if (!displayItem.value || !displayItem.value.location)
-		// 	return '';
-
-		// let temp = '';
-		// if (displayItem.value.location.iteration && displayItem.value.location.iteration.address)
-		// 	// temp = displayItem.value.location.iteration.address.city + ', ' + displayItem.value.location.iteration.address.stateProvince + ' ' + displayItem.value.location.iteration.address.country;
-		// 	temp = AppUtility.address(displayItem.value.location.iteration.address);
-
-		// if (displayItem.value.location.address && (displayItem.value.location.address.city || displayItem.value.location.address.stateProvince || displayItem.value.location.address.country || displayItem.value.location.address.postalCode))
-		// 	// temp = displayItem.value.location.address.city + ', ' + displayItem.value.location.address.stateProvince + ' ' + displayItem.value.location.address.country;
-		// 	temp = AppUtility.address(displayItem.value.location.address);
-
-		// return temp;
 		return launchLocationIterationAddress(displayItem.value);
 	});
 	const displayItemLocationIterationCoords = computed(() => {
-		// if (!displayItem.value || !displayItem.value.location || !displayItem.value.location.iteration)
-		// 	return null;
-
-		// if (displayItem.value.location.iteration.coordsLat || displayItem.value.location.iteration.coordsLong)
-		// 	return [ displayItem.value.location.iteration.coordsLat, displayItem.value.location.iteration.coordsLong ];
-
-		// return null;
 		return launchLocationIterationCoords(displayItem.value);
 	});
 	const displayItemLocationName = computed(() => {
-		// return displayItem.value && displayItem.value.location ? displayItem.value.location.name + displayItemLocationIteration.value : '';
 		return launchLocationName(displayItem.value, displayItemLocationIteration.value);
 	});
 	const displayItemResultsAccelerationDrogue = computed(() => {
-		// if (displayItem.value && displayItem.value.results && displayItem.value.results.accelerationDrogue)
-		// 	return displayItemMeasurementAcceleration(correlationId(), displayItem.value.results, (value) => { return value.accelerationDrogue; }, (value) => { return value.accelerationDrogueMeasurementUnitsId; }, (value) => { return value.accelerationDrogueMeasurementUnitId; });
-		// return null;
 		return launchResultsAccelerationDrogue(displayItem.value);
 	});
 	const displayItemResultsAccelerationMain = computed(() => {
-		// if (displayItem.value && displayItem.value.results && displayItem.value.results.accelerationMain)
-		// 	return displayItemMeasurementAcceleration(correlationId(), displayItem.value.results, (value) => { return value.accelerationMain; }, (value) => { return value.accelerationMainMeasurementUnitsId; }, (value) => { return value.accelerationMainMeasurementUnitId; });
-		// return null;
 		return launchResultsAccelerationMain(displayItem.value);
 	});
 	const displayItemResultsAccelerationMax = computed(() => {
-		// if (displayItem.value && displayItem.value.results && displayItem.value.results.accelerationMax)
-		// 	return displayItemMeasurementAcceleration(correlationId(), displayItem.value.results, (value) => { return value.accelerationMax; }, (value) => { return value.accelerationMaxMeasurementUnitsId; }, (value) => { return value.accelerationMaxMeasurementUnitId; });
-		// return null;
 		return launchResultsAccelerationMax(displayItem.value);
 	});
-	const displayItemResultsAltitudeDrogue = computed(() => {
-		// if (displayItem.value && displayItem.value.results && displayItem.value.results.altitudeDeployDrogue)
-		// 	return displayItemMeasurementAltitude(correlationId(), displayItem.value.results, (value) => { return value.altitudeDeployDrogue; }, (value) => { return value.altitudeDeployDrogueMeasurementUnitsId; }, (value) => { return value.altitudeDeployDrogueMeasurementUnitId; });
-		// return null;
 		return launchResultsAltitudeDrogue(displayItem.value);
 	});
 	const displayItemResultsAltitudeMain = computed(() => {
-		// if (displayItem.value && displayItem.value.results && displayItem.value.results.altitudeDeployMain)
-		// 	return displayItemMeasurementAltitude(correlationId(), displayItem.value.results, (value) => { return value.altitudeDeployMain; }, (value) => { return value.altitudeDeployMainMeasurementUnitsId; }, (value) => { return value.altitudeDeployMainMeasurementUnitId; });
-		// return null;
 		return launchResultsAltitudeMain(displayItem.value);
 	});
 	const displayItemResultsAltitudeMax = computed(() => {
-		// if (displayItem.value && displayItem.value.results && displayItem.value.results.altitudeMax)
-		// 	return displayItemMeasurementAltitude(correlationId(), displayItem.value.results, (value) => { return value.altitudeMax; }, (value) => { return value.altitudeMaxMeasurementUnitsId; }, (value) => { return value.altitudeMaxMeasurementUnitId; });
-		// return null;
 		return launchResultsAltitudeMax(displayItem.value);
 	});
 	const displayItemResultsCoords = computed(() => {
-		// let temp = displayItemResultsCoordsLaunch.value;
-		// if (temp && temp.length > 1)
-		// 	return temp;
-		// temp = displayItemResultsCoordsLocation.value;
-		// if (temp && temp.length > 1)
-		// 	return temp;
-		// temp = displayItemLocationIterationCoords.value;
-		// if (temp && temp.length > 1)
-		// 	return temp;
-		// return null;
 		return launchResultsCoords(displayItem.value);
 	});
 	const displayItemResultsCoordsLatLaunch = computed(() => {
-		// return displayItem.value ? displayItem.value.coordsLatLaunch : '';
 		return launchResultsCoordsLatLaunch(displayItem.value);
 	});
 	const displayItemResultsCoordsLongLaunch = computed(() => {
-		// return displayItem.value ? displayItem.value.coordsLatLaunch : '';
 		return launchResultsCoordsLongLaunch(displayItem.value);
 	});
 	const displayItemResultsCoordsLatRecovery = computed(() => {
-		// return displayItem.value ? displayItem.value.coordsLatLaunch : '';
 		return launchResultsCoordsLatRecovery(displayItem.value);
 	});
 	const displayItemResultsCoordsLongRecovery = computed(() => {
-		// return displayItem.value ? displayItem.value.coordsLatLaunch : '';
 		return launchResultsCoordsLongRecovery(displayItem.value);
 	});
 	const displayItemResultsCoordsLaunch = computed(() => {
-		// return displayItem.value && displayItem.value.results && (displayItem.value.results.coordsLatLaunch || displayItem.value.results.coordsLongLaunch) ? [ displayItem.value.results.coordsLatLaunch, displayItem.value.results.coordsLongLaunch ] : null;
 		return launchResultsCoordsLaunch(displayItem.value);
 	});
 	const displayItemResultsCoordsLocation = computed(() => {
-		// return displayItem.value && (displayItem.value.coordsLat || displayItem.value.coordsLong) ? [ displayItem.value.coordsLat, displayItem.value.coordsLong ] : null;
 		return launchResultsCoordsLocation(displayItem.value);
 	});
 	const displayItemResultsCoordsRecovery = computed(() => {
-		// return displayItem.value && displayItem.value.results && (displayItem.value.results.coordsLatRecovery || displayItem.value.results.coordsLongRecovery) ? [ displayItem.value.results.coordsLatRecovery, displayItem.value.results.coordsLongRecovery ] : null;
 		return launchResultsCoordsRecovery(displayItem.value);
 	});
 	const displayItemResultsVelocityMax = computed(() => {
-		// if (displayItem.value && displayItem.value.results && displayItem.value.results.velocityMax)
-		// 	return displayItemMeasurementVelocity(correlationId(), displayItem.value.results, (value) => { return value.velocityMax; }, (value) => { return value.velocityMaxMeasurementUnitsId; }, (value) => { return value.velocityMaxMeasurementUnitId; });
-		// return null;
 		return launchResultsVelocityMax(displayItem.value);
 	});
 	const displayItemResultsVelocityRecovery = computed(() => {
-		// if (displayItem.value && displayItem.value.results && displayItem.value.results.velocityRecovery)
-		// 	return displayItemMeasurementAcceleration(correlationId(), displayItem.value.results, (value) => { return value.velocityRecovery; }, (value) => { return value.velocityRecoveryMeasurementUnitsId; }, (value) => { return value.velocityRecoveryMeasurementUnitId; });
-		// return null;
 		return launchResultsVelocityRecovery(displayItem.value);
 	});
 	const displayItemRocketMotorNames = computed(() => {
@@ -306,122 +240,54 @@ export function useLaunchViewComponent(props, context, options) {
 		return displayItem.value && displayItem.value.rocketSetup && displayItem.value.rocketSetup.rocket ? displayItem.value.rocketSetup.rocket.name : '';
 	});
 	const displayItemRocketCg = computed(() => {
-		// if (!displayItem.value || !displayItem.value.rocketSetup)
-		// 	return null;
-		// return rocketCgHighest(displayItem.value.rocketSetup.stages);
 		return launchRocketCg(displayItem.value);
 	});
 	const displayItemRocketCp = computed(() => {
-		// if (!displayItem.value || !displayItem.value.rocketSetup || !displayItem.value.rocketSetup.rocket)
-		// 	return null;
-		// return rocketCpHighest(displayItem.value.rocketSetup.rocket.stages);
 		return launchRocketCp(displayItem.value);
 	});
 	const displayItemRocketDiameter = computed(() => {
-		// if (!displayItem.value || !displayItem.value.rocketSetup || !displayItem.value.rocketSetup.rocket)
-		// 	return null;
-		// return rocketDiameterHighest(displayItem.value.rocketSetup.rocket.stages);
 		return launchRocketDiameter(displayItem.value);
 	});
 	const displayItemRocketLength = computed(() => {
-		// if (!displayItem.value || !displayItem.value.rocketSetup || !displayItem.value.rocketSetup.rocket)
-		// 	return null;
-		// return rocketLengthOverall(displayItem.value.rocketSetup.rocket.stages);
 		return launchRocketLength(displayItem.value);
 	});
 	const displayItemRocketWeight = computed(() => {
-		// if (!displayItem.value || !displayItem.value.rocketSetup)
-		// 	return null;
-		// return rocketWeightHighest(displayItem.value.rocketSetup.stages);
 		return launchRocketWeight(displayItem.value);
 	});
 	const displayItemTemperature = computed(() => {
-		// if (displayItem.value && displayItem.value.temperature)
-		// 	return displayItemMeasurementTemperature(correlationId(), displayItem.value, (value) => { return value.temperature; }, (value) => { return value.temperatureMeasurementUnitsId; }, (value) => { return value.temperatureMeasurementUnitId; });
-		// return null;
 		return launchTemperature(displayItem.value);
 	});
 	const displayItemWindSpeed = computed(() => {
-		// if (displayItem.value && displayItem.value.windSpeed)
-		// 	return displayItemMeasurementVelocity(correlationId(), displayItem.value, (value) => { return value.windSpeed; }, (value) => { return value.windSpeedMeasurementUnitsId; }, (value) => { return value.windSpeedMeasurementUnitId; });
-		// return null;
 		return launchWindSpeed(displayItem.value);
 	});
 	const hasCoords = computed(() => {
 		return hasLaunchResultsCoords(displayItem.value);
 	});
 	const hasCoordsLaunch = computed(() => {
-		// if (!displayItem.value || !displayItem.value.results)
-		// 	return false;
-
-		// return (
-		// 	displayItem.value.results.coordsLatLaunch || 
-		// 	displayItem.value.results.coordsLongLaunch
-		// );
 		return hasLaunchResultsCoordsLaunch(displayItem.value);
 	});
 	const hasCoordsLocation = computed(() => {
-		// return (
-		// 	displayItem.value.coordsLat || 
-		// 	displayItem.value.coordsLong
-		// );
 		return hasLaunchResultsCoordsLocation(displayItem.value);
 	});
 	const hasCoordsLocationIteration = computed(() => {
-		// if (!displayItem.value || !displayItem.value.location || !displayItem.value.location.iteration)
-		// 	return false;
-
-		// return (
-		// 	displayItem.value.location.iteration.coordsLat || 
-		// 	displayItem.value.location.iteration.coordsLong
-		// );
 		return hasCoordsLocationIteration(displayItem.value);
 	});
 	const hasCoordsRecovery = computed(() => {
-		// if (!displayItem.value || !displayItem.value.results)
-		// 	return false;
-
-		// return (
-		// 	displayItem.value.results.coordsLatRecovery || 
-		// 	displayItem.value.results.coordsLongRecovery
-		// );
 		return hasLaunchResultsCoordsRecovery(displayItem.value);
 	});
-	const hasResults = computed(() => {
-		// if (!displayItem.value || !displayItem.value.results)
-		// 	return false;
-
-		// return (
-		// 	displayItem.value.results.accelerationMax || 
-		// 	displayItem.value.results.altitudeMax || 
-		// 	displayItem.value.results.altitudeDeployDrogue || 
-		// 	displayItem.value.results.altitudeDeployMain || 
-		// 	displayItem.value.results.velocityMax || 
-		// 	displayItem.value.results.velocityRecovery
-		// );
+	const hasResults = computed(() => {elocityRecovery
 		return hasLaunchResults(displayItem.value);
 	});
 	const hasRocketSpecs = computed(() => {
-		// return displayItemRocketCg.value || displayItemRocketCp.value || displayItemRocketDiameter.value || displayItemRocketLength.value || displayItemRocketWeight.value;
 		return hasLaunchRocketSpecs(displayItem.value);
 	});
 	const hasWeather = computed(() => {
-		// if (!displayItem.value)
-		// 	return false;
-
-		// return (
-		// 	displayItem.value.temperature || 
-		// 	displayItem.value.windSpeed || 
-		// 	(displayItem.value.weather && displayItem.value.weather.length > 0)
-		// );
 		return hasLaunchWeather(displayItem.value);
 	});
 	const isFailure = computed(() => {
-		// return displayItem.value ? displayItem.value.success === AppCommonConstants.Rocketry.Launches.Reasons.Success.failed : false;
 		return isLaunchFailure(displayItem.value);
 	});
 	const isSuccess = computed(() => {
-		// return displayItem.value ? displayItem.value.success === AppCommonConstants.Rocketry.Launches.Reasons.Success.success : false;
 		return isLaunchSuccess(displayItem.value);
 	});
 	
